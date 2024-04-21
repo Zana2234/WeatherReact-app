ee7003b2502be7990b874f0f378450e693d5f390
Ana_Z <113620777+Zana2234@users.noreply.github.com>
2024-04-21 09:06:01 +0200

    added structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Weather from "./Weather.js";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+      <h1> Weather App</h1>
+      <Weather />
+      <footer>
+        This Project was coded by Ana Zabaleta and is{" "}
+        <a href="https://github.com/Zana2234/Weather-react-app.git">
+          open-sourced on GitHub
         </a>
-      </header>
+      </footer>
     </div>
   );
 }
---
+++ src/Weather.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Weather.css";
+
+export default function Weather() {
+  return (
+    <div className="Weather">
+      <ul>
+        <li>Wednesday 07:00</li>
+        <li>Mostly Cloudy</li>
+      </ul>
+      <div className="row">
+        <img src=""></img>
+      </div>
+    </div>
+  );
+}
